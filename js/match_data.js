45efdb081b2d6d9b9e4ccc73feb3d5c81cc3dfc3
xavijam <xavijam@gmail.com>
2014-05-24 18:46:20 +0200

    experiment going on

--- js/match_data.js
+++ js/match_data.js
@@ -1,40 +1,58 @@
 
   var match_data = {
 
-    'Thu Dec 12 2013 20:51:17 GMT+0000': {
-      type: 'injury',
-      by:   'Diego Costa',
-      team: 'Atlético de Madrid'
+    start:  {
+      time: 'Thu Dec 12 2013 20:45:00 GMT+0000',
+      text: '0\''
     },
 
-    'Thu Dec 12 2013 21:15:17 GMT+0000': {
-      type: 'yellow',
-      by:   'Gabi',
-      team: 'Atlético de Madrid'
+    end: {
+      time: 'Thu Dec 12 2013 22:33:00 GMT+0000',
+      text:  '93\''
     },
 
-    'Thu Dec 12 2013 21:21:17 GMT+0000': {
-      type: 'goal',
-      by:   'Ramos',
-      team: 'Real Madrid'
-    },
+    highlights: {
+      'Thu Dec 12 2013 20:51:17 GMT+0000': {
+        type: 'goal',
+        text: '<b>Diego Costa</b> scores a GOAL!.',
+        team: 'Atlético de Madrid'
+      },
 
-    'Thu Dec 12 2013 22:11:17 GMT+0000': {
-      type: 'red',
-      by:   'Casillas',
-      team: 'Real Madrid'
-    },
+      'Thu Dec 12 2013 21:15:17 GMT+0000': {
+        type: 'yellow',
+        text: '<b>Gabi</b> is shown the yellow card',
+        team: 'Atlético de Madrid'
+      },
 
-    'Thu Dec 12 2013 22:12:17 GMT+0000': {
-      type: 'substitution',
-      by:   'Benzema by Diego López',
-      team: 'Real Madrid'
-    },
+      'Thu Dec 12 2013 21:21:17 GMT+0000': {
+        type: 'goal',
+        text: 'GOAL by <b>Ramos</b>!',
+        team: 'Real Madrid'
+      },
 
-    'Thu Dec 12 2013 22:22:17 GMT+0000': {
-      type: 'substitution',
-      by:   'Koke by Diego',
-      team: 'Atlético de Madrid'
+      'Thu Dec 12 2013 22:00:17 GMT+0000': {
+        type: 'red',
+        text: '<b>Iker Casillas</b> is shown the red card',
+        team: 'Real Madrid'
+      },
+
+      'Thu Dec 12 2013 22:15:17 GMT+0000': {
+        type: 'substitution',
+        text: '<b>Diego López</b> replaces <b>Benzema</b>',
+        team: 'Real Madrid'
+      },
+
+      'Thu Dec 12 2013 22:21:17 GMT+0000': {
+        type: 'goal',
+        text: 'GOAL by <b>Ramos</b>!',
+        team: 'Real Madrid'
+      },
+
+      'Thu Dec 12 2013 22:32:17 GMT+0000': {
+        type: 'substitution',
+        text:  '<b>Diego</b> replaces <b>Koke</b>',
+        team: 'Atlético de Madrid'
+      }
     }
 
   }
