7f6bddc8e40ac23bd5811de829cbdf39752ab1b0
xavijam <xavijam@gmail.com>
2014-05-25 16:28:52 +0200

    tooltips enabled

--- js/match.js
+++ js/match.js
@@ -1,6 +1,8 @@
 
-
+  var URL = 'http://cartodb.github.io/lisbon-2014/#/';
+  var SHARE_TEXT = 'Champions League 2014 Tweets - ';
   var torqueLayer;
+  var map;
   var count = 1;
 
   function main() {
@@ -11,8 +13,72 @@
       return slider.render();
     });
 
+    // share content
+    cdb.vis.Overlay.register('share', function(data, vis) {
 
-    // add a nice baselayer from Stamen 
+      // Add the complete url for facebook and twitter
+      if (location.href) {
+        data.share_url = encodeURIComponent(location.href);
+      } else {
+        data.share_url = data.url;
+      }
+
+      var template = cdb.core.Template.compile(
+        data.template || '\
+          <div class="mamufas">\
+            <div class="block modal {{modal_type}}">\
+              <a href="#close" class="close">x</a>\
+              <div class="head">\
+                <h3>Share this map</h3>\
+              </div>\
+              <div class="content">\
+                <div class="buttons">\
+                  <h4>Social</h4>\
+                  <ul>\
+                    <li><a class="facebook" target="_blank" href="{{ facebook_url }}">Share on Facebook</a></li>\
+                    <li><a class="twitter" href="{{ twitter_url }}" target="_blank">Share on Twitter</a></li>\
+                    <li><a class="link" href="{{ public_map_url }}" target="_blank">Link to this map</a></li>\
+                  </ul>\
+                </div><div class="embed_code">\
+                 <h4>Embed this map</h4>\
+                 <textarea id="" name="" cols="30" rows="10">{{ code }}</textarea>\
+               </div>\
+              </div>\
+            </div>\
+          </div>\
+        ',
+        data.templateType || 'mustache'
+      );
+
+      var url = location.href;
+
+      url = url.replace("public_map", "embed_map");
+
+      var public_map_url = url.replace("embed_map", "public_map"); // TODO: get real URL
+
+      var code = "<iframe width='100%' height='520' frameborder='0' src='" + url + "' allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>";
+
+      var dialog = new cdb.ui.common.ShareDialog({
+        title:        "Real Madrid v Atlético Madrid: how the #UCL final played out on Twitter",
+        description:  "Visualization about the amount of tweets during 2014 Champions League final between Real Madrid and Atlético de Madrid",
+        model: vis.map,
+        code: code,
+        url: data.url,
+        public_map_url: public_map_url,
+        share_url: data.share_url,
+        template: template,
+        target: $(".cartodb-share a"),
+        size: $(document).width() > 400 ? "" : "small",
+        width: $(document).width() > 400 ? 430 : 216
+      });
+
+      return dialog.render();
+
+    });
+
+
+
+
 
     cartodb.createVis('map', 'http://srogers.cartodb.com/api/v2/viz/a43ea07a-e3f2-11e3-92f9-0e230854a1cb/viz.json', {
       center_lat: 24.5,
@@ -28,7 +94,7 @@
           maxZoom: 10
         });
 
-        var map = vis.getNativeMap();
+        map = vis.getNativeMap();
         var layer = layers[2];
 
         var hash = new L.Hash(map, layer);
@@ -102,7 +168,7 @@
 
           $('.highlights').find('.' + block + ' .timeline').append(d.el);
 
-          generateTooltip(d.el.find('i'));
+          generateTooltip(d, i);
 
           if (d.type === "goal") {
             var goals = parseInt($('em.' + block).text());
@@ -119,9 +185,9 @@
         }
 
         if (d.el) {
-          d.el
-            .hide()
-            .remove();
+          d.el.hide();
+          destroyTooltip(d.el.find('i'));
+          d.el.remove();
 
           delete d.el;
         }
@@ -131,39 +197,68 @@
   }
 
   function destroyTooltip($el) {
-
+    $el.unbind('click');
+    var tipsy = $el.data('tipsy');
+    if (tipsy.$tip) {
+      tipsy.$tip.remove();
+    }
   }
 
-  function generateTooltip($el) {
+  function closeTipsys() {
+    $('i').each(function() {
+      if ($(this).data('tipsy')) {
+        $(this).tipsy('hide');
+      }
+    });
+  }
+
+  function getURL(timestamp) {
+    var tb = torqueLayer.getTimeBounds();
+    var step = Math.floor(((timestamp-tb.start)*tb.steps)/(tb.end-tb.start));
+    var center = map.getCenter();
+    return encodeURIComponent(URL + map.getZoom() + "/" + center.lat + "/" + center.lng + "/" + step)
+  }
+
+  function getShareText(d) {
+    return SHARE_TEXT + d.text.replace(/<(?:.|\n)*?>/gm, '');
+  }
+
+  function generateTooltip(d, t) {
+
+    var $el = d.el.find('i')
+    var url = getURL(new Date(t).getTime());
     
-    // $el.tipsy({
-    //   // trigger:  'manual',
-    //   title:    function() {
-    //     return this.getAttribute('data-tipsy') + ''
-    //   },
-    //   html:     true,
-    //   gravity:  $.fn.tipsy.autoBounds(250, 's'),
-    //   fade:     true,
-    //   delayIn:  300,
-    //   delayOut: 750
-    // })
+    $el.tipsy({
+      trigger:  'manual',
+      title:    function() {
+        return this.getAttribute('data-tipsy') +
+        '<div class="highlight-share">' + 
+        '<p>Share this moment</p>' + 
+        '<span class="buttons">' +
+        '<a target="_blank" href="http://www.facebook.com/sharer.php?u=' + url + '&text=' + getShareText(d) + '" class="facebook"></a>'+
+        '<a target="_blank" href="https://twitter.com/share?url=' + url + '&text=' + getShareText(d) + '" class="twitter"></a>'+
+        '</span>'+
+        '</div>'
+      },
+      html:     true,
+      gravity:  $.fn.tipsy.autoBounds(250, 's'),
+      fade:     true,
+      delayIn:  300,
+      delayOut: 750
+    })
 
-    // $('#mylink').attr('title','Input here:<input id="toolbar">');
-    // $('#mylink').tipsy({trigger:'manual',gravity:'w', html:true});
+    $el.click(function(){
+      closeTipsys();
 
-    // //.tipsy class is what the generated tooltip divs have, so we use the 
-    // //live event to link the mouseover/mouseout events
-    // $('.tipsy').live('mouseover',function(e){
-    //   clearTimeout(timer);
-    // });
-    // $('.tipsy').live('mouseout',function(e){
-    //   timer = setTimeout("$('#mylink').tipsy('hide');",3000);//hide the link in 3 seconds
-    // });
-
-    // //manually show the tooltip
-    // $('#mylink').bind('mouseover',function(e){
-    //    $(this).tipsy('show');
-    //  });
+      var $i = $(this);
+      $i.tipsy('show');
+      
+      setTimeout(function() {
+        $i.data('tipsy').$tip.bind('mouseleave', function(){
+          $i.tipsy('hide');
+        });
+      },600);
+    })
   }
 
   function getHighlightPos(timestamp)  {
